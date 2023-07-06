4d113a3c8f6a49c72e0a87baa6202d502d6aed42
emhaki <mhmh779@naver.com>
2023-07-06 20:39:42 +0900

    대출 도메인 프로젝트

--- src/main/java/com/fastcampus/loan/service/EntryService.java
+++ src/main/java/com/fastcampus/loan/service/EntryService.java
@@ -3,7 +3,7 @@ package com.fastcampus.loan.service;
 import com.fastcampus.loan.dto.EntryDTO.Request;
 import com.fastcampus.loan.dto.EntryDTO.Response;
 import com.fastcampus.loan.dto.EntryDTO.UpdateResponse;
-
+//
 public interface EntryService {
 
   Response create(Long applicationId, Request request);
