217c3e932184fbe9ffee2d204242350c32021063
emhaki <mhmh779@naver.com>
2023-08-19 13:33:38 +0900

    Counsel 도메인 정의

--- src/main/java/com/fastcampus/loan/domain/Counsel.java
+++ src/main/java/com/fastcampus/loan/domain/Counsel.java
@@ -1,19 +1,13 @@
 package com.fastcampus.loan.domain;
 
-import java.time.LocalDateTime;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -31,27 +25,25 @@ public class Counsel extends BaseEntity {
   @Column(nullable = false, updatable = false)
   private Long counselId;
 
-  @Column(columnDefinition = "datetime DEFAULT NULL COMMENT '신청일자'")
+  @Column(nullable = false, columnDefinition = "datetime COMMENT '신청일자'")
   private LocalDateTime appliedAt;
 
-  @Column(columnDefinition = "varchar(12) DEFAULT NULL COMMENT '상담 요청자'")
+  @Column(nullable = false, columnDefinition = "varchar(12) COMMENT '상담 요청자'")
   private String name;
 
-  @Column(columnDefinition = "varchar(13) DEFAULT NULL COMMENT '전화번호'")
+  @Column(nullable = false, columnDefinition = "varchar(23) COMMENT '전화번호'")
   private String cellPhone;
 
-  @Column(columnDefinition = "varchar(50) DEFAULT NULL COMMENT '책임자 이메일'")
-  private String email;
-
-  @Column(columnDefinition = "text DEFAULT NULL COMMENT '상담 메모'")
+  @Column(columnDefinition = "varchar(50) DEFAULT NULL COMMENT '상담 요청자 이메일'")
   private String memo;
 
   @Column(columnDefinition = "varchar(50) DEFAULT NULL COMMENT '주소'")
   private String address;
 
-  @Column(columnDefinition = "varchar(50) DEFAULT NULL COMMENT '주소 상세'")
+  @Column(columnDefinition = "varchar(50) DEFAULT NULL COMMENT '상세 주소'")
   private String addressDetail;
 
   @Column(columnDefinition = "varchar(5) DEFAULT NULL COMMENT '우편번호'")
   private String zipCode;
+
 }
