42fb2590e3e10025e7c959aa1d7dfc23b5d19fa7
emhaki <mhmh779@naver.com>
2023-09-01 23:30:31 +0900

    fileDTO생성

--- src/main/java/com/fastcampus/loan/exception/ResultType.java
+++ src/main/java/com/fastcampus/loan/exception/ResultType.java
@@ -7,7 +7,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResultType {
     SUCCESS("0000", "success"),
+
+    NOT_EXIST("4001", "file not exist"),
     SYSTEM_ERROR("9000", "system error");
+
+
 
     private final String code;
     private final String desc;
