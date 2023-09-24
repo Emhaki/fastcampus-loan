6f6765d5885c8c72e1ef8464e110da59c837867e
emhaki <mhmh779@naver.com>
2023-09-24 20:39:51 +0900

    delete

--- src/main/java/com/fastcampus/loan/service/EntryServiceImpl.java
+++ src/main/java/com/fastcampus/loan/service/EntryServiceImpl.java
@@ -79,6 +79,24 @@ public class EntryServiceImpl implements EntryService {
         return EntryDTO.UpdateResponse.builder().applicationId(applicationId).beforeEntryAmount(beforeEntryAmount).afterEntryAmount(request.getEntryAmount()).build();
     }
 
+    @Override
+    public void delete(Long entryId) {
+        Entry entry = entryRepository.findById(entryId).orElseThrow(() -> new BaseException(ResultType.SYSTEM_ERROR));
+
+        entry.setIsDeleted(true);
+
+        entryRepository.save(entry);
+
+        BigDecimal beforeEntryAmount = entry.getEntryAmount();
+
+        Long applicationId = entry.getApplicationId();
+        balanceService.update(applicationId,
+                BalanceDTO.UpdateRequest.builder()
+                        .beforeEntryAmount(beforeEntryAmount)
+                        .afterEntryAmount(BigDecimal.ZERO)
+                        .build());
+    }
+
     private boolean isContractedApplication(Long applicationId) {
         Optional<Application> existed = applicationRepository.findById(applicationId);
 
